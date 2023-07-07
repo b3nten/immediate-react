a379004b88fab2db8b72c8a6062af25fd1e11af5
Benten <ben@benten.ca>
2023-07-07 02:23:46 -0600

    Add ad

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,24 @@ export default class App extends Immediate {
   view() {
     return (
       <div className="is-center" style={{ flexDirection: "column" }}>
+        <h1>Immediate</h1>
+        <p>This is all the code required to create the following interactive UI.</p>
+        <pre>
+          {`let count = 0;
+
+export default class App extends Immediate {
+  view() {
+    return (
+      <div>
         <h1>Hello World</h1>
+        <button onClick={() => count++}>Click Me {count}</button>
+        <div>{new Date().toLocaleTimeString()}</div>
+        <div>{window.innerWidth} x {window.innerHeight}</div>
+      </div>
+    );
+  }
+}`}
+        </pre>
         <button onClick={() => count++}>Click Me {count}</button>
         <div>{new Date().toLocaleTimeString()}</div>
         <div>{window.innerWidth} x {window.innerHeight}</div>
